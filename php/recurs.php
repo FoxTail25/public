ce6dc45baa836a33993126d6be8711e251ba4d3f
Grigoriy <foxtail25@gmail.com>
2025-04-01 09:31:59 +0300

    rec2

--- php/recurs.php
+++ php/recurs.php
@@ -15,6 +15,8 @@ addBr(rB('Recusion'));
 // }
 // func();
 
+
+//С помощью рекурсии найдите выведите все элементов этого массива.
 $arr = ['a' => 1, 'b' => 2, 'c'=> 3, 'd' => 4, 'e' => 5];
 
 function recArr($arr){
@@ -24,6 +26,51 @@ function recArr($arr){
 	}
 }
 recArr($arr);
+
+$arr = ['a' => 1, 'b' => 2, 'c' => 3, 'd' => 4, 'e' => 5]; 
+//С помощью рекурсии найдите сумму элементов этого массива.
+function summArr($arr){	
+	$summ = array_shift($arr);
+
+	if(count($arr) !== 0) {
+		$summ += summArr($arr);
+	}
+	return$summ;
+}
+
+echo summArr(($arr));
+
+// Дан многомерный массив произвольного уровня вложенности, например, такой: С помощью рекурсии выведите все примитивные элементы этого массива на экран.
+
+$arr = [1, 2, 3, [4, 5, [6, 7]], [8, [9, 10]]];
+
+function showElInMultiArr($arr){
+	foreach($arr as $el){
+		if(is_array($el)){
+			showElInMultiArr($el);
+		}else{
+			echo $el. '<br/>';
+		}
+	}
+}
+showElInMultiArr($arr);
+
+// Дан многомерный массив произвольного уровня вложенности, например, такой: С помощью рекурсии найдите сумму элементов этого массива.
+$arr = [1, 2, 3, [4, 5, [6, 7]], [8, [9, 10]]];
+function summElinMiltiArr($arr){
+	$summ=0;
+	foreach($arr as $el){
+		if(is_array($el)){
+			$summ += summElinMiltiArr($el);
+		}else{
+			$summ += $el;
+		}
+	}
+	return $summ;
+}
+
+echo summElinMiltiArr($arr);
+
 
 
 ?>
