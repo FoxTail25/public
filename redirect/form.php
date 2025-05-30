7db9e3398b66e8f985bf437f034fb872652b4e1f
Grigoriy <foxtail25@gmail.com>
2025-05-30 09:37:39 +0300

    redir_form

--- redirect/form.php
+++ redirect/form.php
@@ -1,10 +1,15 @@
 <?php
 if(!empty($_POST)){
-	
+	file_put_contents('111test.txt', $_POST['test1']);
+	file_put_contents('222test.txt', $_POST['test2']);
+    header('location:form.php');
+    // echo "da!";
 }
 ?>
-<form method="POST">
+<form method="POST" style="display:grid; width:50%;">
+тест1
 <input name="test1">
+тест2
 <input name="test2">
 <input type="submit">
 </form>
