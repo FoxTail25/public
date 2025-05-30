d8f581f14bcecc8231353dda4f7e607316d9690b
Grigoriy <foxtail25@gmail.com>
2025-05-30 09:47:22 +0300

    redir_db

--- redirect/form.php
+++ redirect/form.php
@@ -1,4 +1,11 @@
 <?php
+
+$host = "MySQL-8.0";
+$user = "root";
+$pass = "";
+$dbname = 'db_pract';
+$db_pract_link = mysqli_connect($host, $user, $pass, $dbname);
+
 if(!empty($_POST)){
 	file_put_contents('111test.txt', $_POST['test1']);
 	file_put_contents('222test.txt', $_POST['test2']);
