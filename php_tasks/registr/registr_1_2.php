8ddda88e6a27d3f87583f839e8e7627c7b2dc969
Fox Tail <foxtail25@gmail.com>
2025-06-07 18:44:58 +0300

    register_22

--- php_tasks/registr/registr_1_2.php
+++ php_tasks/registr/registr_1_2.php
@@ -6,38 +6,48 @@ session_start();
 <form method="post" style="display: grid; width:200px;">
 	login:
 	<input type="text" name="login">
-	passqord:
+	password:
 	<input type="password" name="password">
-    email:
+	email:
 	<input type="email" name="email">
-    BirthDate:
+	BirthDate:
 	<input type="date" name="birthdate">
 
 	<input type="submit">
 </form>
 
 <?php
-if (!empty($_POST['login']) and !empty($_POST['password'])) {
+if (!empty($_POST['login']) and !empty($_POST['password']) and !empty($_POST['email']) and !empty($_POST['birthdate'])) {
 	include('../../db/connect.php');
 	$login = $_POST['login'];
 	$password = $_POST['password'];
-    $email = $_POST['email'];
-    $birthdate = $_POST['birthdate'];
-	$query = "INSERT INTO user_auth SET login='$login', password = '$password'";
-	// $query_add_email = "INSERT INTO user_email SET login='$email', user_id ='$user_id'";
-	$answer = mysqli_query($db_pract_link, $query);
+	$email = $_POST['email'];
+	$birthDate = $_POST['birthdate'];
+	$query = "INSERT INTO user_auth SET login='$login', password = '$password', birth_date='$birthDate'";
+	mysqli_query($db_pract_link, $query);
+	
+	$query_user_id = "SELECT id FROM user_auth WHERE login = '$login'";
+	$res = mysqli_query($db_pract_link, $query_user_id);
+	for ($data = []; $row = mysqli_fetch_assoc($res); $data[] = $row);
+	$userId = $data[0]["id"];
+
+	$query_add_email = "INSERT INTO user_email SET email='$email', user_id ='$userId'";
+	mysqli_query($db_pract_link, $query_add_email);
+	// $query_add_birth_date = "INSERT INTO user_birth_date SET birth_date='$birthDate', user_id ='$userId'";
+	// mysqli_query($db_pract_link, $query_add_birth_date);
+
 	$_SESSION['success'] = true;
 	unset($_POST);
-    var_dump($answer);
-	// header('location:#');
+	header('location:#');
 }
 ?>
 <?php
 if (isset($_SESSION['success'])): ?>
 	<p>Пользователь успешно добавлен в базу</p>
-	<a href="../index.php#register">вернуться на главную</a>
+	<a href="../../index.php#register2">вернуться на главную</a>
+<?php else : ?>
+	<br />
+	<a href="../../index.php#register2">На главную</a>
 <?php endif;
 $_SESSION['success'] = null;
 ?>
-<br/>
-<a href="../../index.php">На главную</a>
