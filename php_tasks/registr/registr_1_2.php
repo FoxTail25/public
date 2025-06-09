603339f86dc19cf3653714375a8170b0758fec43
Fox Tail <foxtail25@gmail.com>
2025-06-09 13:18:11 +0300

    register_31

--- php_tasks/registr/registr_1_2.php
+++ php_tasks/registr/registr_1_2.php
@@ -1,8 +1,34 @@
 <?php
 session_start();
-?>
-<p>Регистрация нового пользователя</p>
 
+if (!empty($_POST['login']) and !empty($_POST['password']) and !empty($_POST['email']) and !empty($_POST['birthdate'])) {
+	include('../../db/connect.php');
+	$login = $_POST['login'];
+	$password = $_POST['password'];
+	$email = $_POST['email'];
+	$birthDate = $_POST['birthdate'];
+	$timestamp = date('Y-m-d H:i:s');
+	$query = "INSERT INTO user_auth SET login='$login', password = '$password', birth_date='$birthDate', register_date='$timestamp'";
+	mysqli_query($db_pract_link, $query);
+
+	$query_user_id = "SELECT id FROM user_auth WHERE login = '$login'";
+	$res = mysqli_query($db_pract_link, $query_user_id);
+	for ($data = []; $row = mysqli_fetch_assoc($res); $data[] = $row);
+	$userId = $data[0]["id"];
+
+	$query_add_email = "INSERT INTO user_email SET email='$email', user_id ='$userId'";
+	mysqli_query($db_pract_link, $query_add_email);
+
+
+	$_SESSION['success'] = true;
+	unset($_POST);
+	// echo "success";
+	header('location:#');
+	die();
+}
+?>
+
+<p>Регистрация нового пользователя</p>
 <form method="post" style="display: grid; width:200px;">
 	login:
 	<input type="text" name="login">
@@ -16,30 +42,7 @@ session_start();
 	<input type="submit">
 </form>
 
-<?php
-if (!empty($_POST['login']) and !empty($_POST['password']) and !empty($_POST['email']) and !empty($_POST['birthdate'])) {
-	include('../../db/connect.php');
-	$login = $_POST['login'];
-	$password = $_POST['password'];
-	$email = $_POST['email'];
-	$birthDate = $_POST['birthdate'];
-	$query = "INSERT INTO user_auth SET login='$login', password = '$password', birth_date='$birthDate'";
-	mysqli_query($db_pract_link, $query);
-	
-	$query_user_id = "SELECT id FROM user_auth WHERE login = '$login'";
-	$res = mysqli_query($db_pract_link, $query_user_id);
-	for ($data = []; $row = mysqli_fetch_assoc($res); $data[] = $row);
-	$userId = $data[0]["id"];
 
-	$query_add_email = "INSERT INTO user_email SET email='$email', user_id ='$userId'";
-	mysqli_query($db_pract_link, $query_add_email);
-
-
-	$_SESSION['success'] = true;
-	unset($_POST);
-	header('location:#');
-}
-?>
 <?php
 if (isset($_SESSION['success'])): ?>
 	<p>Пользователь успешно добавлен в базу</p>
@@ -48,5 +51,6 @@ if (isset($_SESSION['success'])): ?>
 	<br />
 	<a href="../../index.php#register2">На главную</a>
 <?php endif;
+
 $_SESSION['success'] = null;
 ?>
