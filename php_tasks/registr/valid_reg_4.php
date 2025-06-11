5b8143dd9572a766c5b41266958a20824e756105
Grigoriy <foxtail25@gmail.com>
2025-06-11 10:06:44 +0300

    register_4_5

--- php_tasks/registr/valid_reg_4.php
+++ php_tasks/registr/valid_reg_4.php
@@ -2,17 +2,21 @@
 session_start();
 
 if (!empty($_POST)) {
-	if (checkLog()['test']) {
+	if (checkLog()['test']) { //проверка логина
 
-		if (checkPass()['test']) {
+		if (checkPass()['test']) { //проверка пароля
 
-			if (!empty($_POST['confirm'])) {
+			if (checkConfirm()['test']) {//проверка подтверждения пароля
 
-				if ($_POST['password'] == $_POST['confirm'] and strlen($_POST['password']) == strlen($_POST['confirm'])) {
+				if ($_POST['password'] == $_POST['confirm'] and strlen($_POST['password']) == strlen($_POST['confirm'])) {//проверка совпадения паролей
 					echo 'password и password confirm совпадают<br/>';
 					echo 'Можно добалять в базу';
 
-					//Код добавления в базу.
+					if(checkEmail()['test']) {//проверка email
+
+						//Код добавления в базу.
+					}
+
 
 				} else {
 					echo 'password и password confirm не совпадают';
@@ -35,6 +39,8 @@ if (!empty($_POST)) {
 	<input type="password" name="password" value="<?= isset($_POST['password']) ? $_POST['password'] : '' ?>">
 	confirm password:
 	<input type="password" name="confirm" value="<?= isset($_POST['confirm']) ? $_POST['confirm'] : '' ?>">
+	confirm email:
+	<input type="email" name="email" value="<?= isset($_POST['emai']) ? $_POST['email'] : '' ?>">
 	<br />
 	<input type="submit">
 </form>
@@ -82,11 +88,34 @@ function checkPass()
 		return ['test' => false, 'msg' => " не заполнен<br/>"];
 	}
 
-	$passLength = strlen(trim($pass));
+	$passLength = strlen($pass);
 	if ($passLength < 6) { // проверка что бы не был слишком короткий
 		return ['test' => false, 'msg' => " слишком короткий<br/>"];
 	}
 	if ($passLength > 12) { // проверка что бы не был слишком длинный
+		return ['test' => false, 'msg' => " слишком длинный<br/>"];
+	}
+
+	return ['test' => true, 'msg' => " подходит<br/>"]; // все проверки прошли успешно.
+}
+
+function checkConfirm()
+{
+	$_POST['confirm'] = trim($_POST['confirm']);
+	$confirm = $_POST['confirm'];
+
+	if (empty($confirm)) { // проверка на заполненность поля и 0
+		if (empty($confirm) and $confirm == 0) {
+			return ['test' => false, 'msg' => " слишком короткий<br/>"];
+		}
+		return ['test' => false, 'msg' => " не заполнен<br/>"];
+	}
+
+	$confirmLength = strlen($confirm);
+	if ($confirmLength < 6) { // проверка что бы не был слишком короткий
+		return ['test' => false, 'msg' => " слишком короткий<br/>"];
+	}
+	if ($confirmLength > 12) { // проверка что бы не был слишком длинный
 		return ['test' => false, 'msg' => " слишком длинный<br/>"];
 	}
 
